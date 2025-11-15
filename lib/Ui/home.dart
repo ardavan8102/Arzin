72371ed4feb74443de1fc31567585476c1879c14
Ardavan Eskandari <eskandariardavan81@gmail.com>
2025-11-16 01:57:23 +0330

    font added

---
+++ lib/Ui/home.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        actions: [
+          const SizedBox(width: 12), // Space from Left
+
+          Expanded(
+            child: Align(
+              alignment: AlignmentGeometry.centerLeft,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.menu),
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: AlignmentGeometry.centerRight,
+            child: const Text(
+              'قیمت بروز نرخ ارز',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+
+          const SizedBox(width: 6),
+
+          Image.asset('assets/img/icon-appbar.png'),
+
+          const SizedBox(width: 12), // Space from Right
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+        
+                Text(
+                  'نرخ ارز آزاد چیست؟',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+
+                SizedBox(width: 8),
+        
+                Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.amberAccent.withValues(alpha: 0.3),
+                  ),
+                  child: Icon(
+                    Icons.question_mark,
+                    color: Colors.amberAccent,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 12),
+
+            Text(
+              'نرخ ارزها در معاملات نقدی و رایج روازانه است معاملات نقدی معاملاتی هستند که خریدار و فروشنده به محض انجام معامله ارز و ریال را باهم تبادل میکنند',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+              textDirection: TextDirection.rtl,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
